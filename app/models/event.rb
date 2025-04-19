06ee23b3081a225489a31fb309ffe9f6597315a6
Dulcelene Machado <dudutola@gmail.com>
2025-04-19 10:12:04 +0100

    Added validations to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,14 @@
 class Event < ApplicationRecord
+  validates :title, presence: true
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
+
+  private
+
+  def event_ends_after_starts
+    if starts_at.present? && ends_at.present? && ends_at <= starts_at
+      errors.add(:ends_at, "must be after the start time")
+    else
+    end
+  end
 end
