5e8e500015af0881683f6551b05ed9831ccfca74
Dulcelene Machado <dudutola@gmail.com>
2025-04-20 14:24:09 +0100

    Added timezone configuration with standard and daylight settings for iCalendar

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,14 +7,36 @@ class Event < ApplicationRecord
   def to_icalendar
     cal = Icalendar::Calendar.new
 
+    tz = Icalendar::Timezone.new
+    tz.tzid = "Europe/Lisbon"
+
+    # STANDARD time (winter time)
+    standard = Icalendar::Timezone::Standard.new
+    standard.dtstart = "20231029T020000"
+    standard.tzoffsetfrom = "+0100"
+    standard.tzoffsetto = "+0000"
+    standard.tzname = "WET"
+    tz.add_standard(standard)
+
+    # DAYLIGHT time (summer time)
+    daylight = Icalendar::Timezone::Daylight.new
+    daylight.dtstart = "20240331T010000"
+    daylight.tzoffsetfrom = "+0000"
+    daylight.tzoffsetto = "+0100"
+    daylight.tzname = "WEST"
+    tz.add_daylight(daylight)
+
+    cal.add_timezone(tz)
+
     cal.event do |e|
       e.dtstart       = Icalendar::Values::DateTime.new(starts_at, "tzid" => "Europe/Lisbon")
       e.dtend         = Icalendar::Values::DateTime.new(ends_at, "tzid" => "Europe/Lisbon")
       e.summary       = title
-      e.description   = description
+      e.description   = description || ""
       e.ip_class      = "PRIVATE"
       e.uid           = "event-#{self.id}@localhost"
-      e.sequence      = Time.now.to_i
+      # e.sequence      = Time.now.to_i
+      e.sequence = updated_at.to_i if updated_at.present?
       e.dtstamp       = Time.now.utc
     end
 
